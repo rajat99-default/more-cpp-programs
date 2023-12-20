3ef6ae64a156153db73c9028dd2d2ba4b7a01ab2
Rajat Sujane <rajatsujane6@gmail.com>
2023-12-20 08:40:37 +0530

    changes

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
+
+class rajat
+{
+    public:
+    int get(int x,int y)
+    {
+        return x,y;
+    }
+
+};
+
+rajat obj;
+
 int main()
 {
-    cout<<"hello";
+    // cout<<"hello";
+    cout<<obj.get(23,24);
     return 0;
 }
